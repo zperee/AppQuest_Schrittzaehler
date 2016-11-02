9a18f5d8e04e28503817f30c965d6f89859d4846
zmartl <luca.marti@bluewin.ch>
2016-11-02 14:52:16 +0100

    Scan Action Listener

---
+++ AppQuest_Schrittzaehler/AppQuest_Schrittzaehler/Pages/ScannerPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AppQuest_Schrittzaehler.Model;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace AppQuest_Schrittzaehler.Pages
+{
+    public partial class ScannerPage : ContentPage
+    {
+        private readonly Route _route;
+
+        public ScannerPage(Route route)
+        {
+            _route = route;
+            InitializeComponent();
+        }
+
+        private async void ScanButton_OnClicked(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += result =>
+            {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                //TODO Add Scanned Element to List
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(() => { Navigation.PopModalAsync(); });
+            };
+
+            // Navigate to our scanner page
+            await Navigation.PushModalAsync(scanPage);
+        }
+    }
+}
