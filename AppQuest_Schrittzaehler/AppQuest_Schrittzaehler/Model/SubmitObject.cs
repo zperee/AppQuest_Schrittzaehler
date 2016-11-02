7c3470be770c9c271e6f627d4b4c8aab3e41b49a
zmartl <luca.marti@bluewin.ch>
2016-11-02 14:09:08 +0100

    Init commit

---
+++ AppQuest_Schrittzaehler/AppQuest_Schrittzaehler/Model/SubmitObject.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace AppQuest_Schrittzaehler.Model
+{
+    public class SubmitObject
+    {
+        [JsonProperty("task")]
+        public string Task => "Schrittzaehler";
+
+        [JsonProperty("startStation")]
+        public int StartStation { get; set; }
+
+        [JsonProperty("endStation")]
+        public int EndStation { get; set; }
+    }
+}
